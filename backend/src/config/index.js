114882c6c0dbc7225358f662d020badb77ddb9a9
a-golosnyak <andrey_g.pt@mail.ru>
2021-08-06 00:19:47 +0300

    React application live reload not working on Windows environment.'

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -8,7 +8,7 @@ const baseConfig = {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 3000,
+  port: 3001,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d'
